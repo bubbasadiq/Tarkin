c6f6425583b1d72711469037bb50765a8a9017e8
najiblere <kabir.najib@wayapaychat.com>
2022-10-06 00:13:12 +0100

    diasble eslint

--- next.config.js
+++ next.config.js
@@ -4,8 +4,12 @@ const nextConfig = {
   swcMinify: true,
   images: {
     unoptimized: true
-    }
-
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
 }
 
 module.exports = nextConfig
